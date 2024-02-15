8f7bedf89c912ea722981afb7c7e3d13019094f5
amrfad <amr162005@gmail.com>
2024-02-15 11:20:47 +0700

    soal-soal nomor berikutnya

---
+++ Soal3.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class Soal3 {
+    public static void main(String[] args){
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+
+        System.out.println("Nilai nx setelah digunakan Math.round(x): "+nx);
+    }
+}
